4c2d7e80c23155ac5fbb4f6dfcc218683c36680f
Evan Wheeler <anon0mys87@gmail.com>
2018-07-18 21:49:59 -0600

    Add webmock for testing external requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,9 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+require 'webmock/rspec'
+WebMock.allow_net_connect!
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
