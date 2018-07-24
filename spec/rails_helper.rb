6ad1b3c3131fcb145c657610f661458510049b27
agpiermarini <piermarini.andrew@gmail.com>
2018-07-24 13:29:30 -0600

    Complete react on rails installation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,10 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  # Ensure that if we are running js tests, we are using latest webpack assets
+  # This will use the defaults of :js and :server_rendering meta tags
+  ReactOnRails::TestHelper.configure_rspec_to_compile_assets(config)
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
