f302c19686b0da44918a7df9dfee69f472795bf6
agpiermarini <piermarini.andrew@gmail.com>
2018-07-26 20:27:37 -0600

    Add flash message for unsuccessful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,11 @@ class SessionsController < ApplicationController
     user = User.new(user_params)
     service = ScavengrBackend::Users.new()
     response = service.authenticate(user)
-    session[:current_user] = response
-    flash[:success] = 'Successfully logged in'
+    if response
+      session[:current_user] = response
+    else
+      flash[:error] = 'Incorrect username or password'
+    end
     redirect_to root_path
   end
 
