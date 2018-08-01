b382efe43ae51cd987d73451f78e7e398c83bb65
Evan Wheeler <anon0mys87@gmail.com>
2018-07-31 22:30:58 -0600

    Add error handling and flash messages for invalid logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :authenticate!
 
   def current_user
-    @user ||= User.new(session[:current_user]) if session[:current_user]
+    begin
+      @user ||= User.new(session[:current_user]) if session[:current_user]
+    rescue ActiveModel::UnknownAttributeError
+      flash[:fail] = "Invalid username or password"
+      session.clear
+      redirect_to root_path
+    end
   end
 
   def authenticate!
