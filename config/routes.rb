e5c1ed8443359fcd3129339aa8e1c2eb0df5a49f
Evan Wheeler <anon0mys87@gmail.com>
2018-07-31 20:57:18 -0600

    Remove /login path and sessions new actions, move login to root path if no current user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get '/create_account', to: 'users#new', as: 'create_account'
-  get '/login', to: 'sessions#new', as: 'login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   post '/login', to: 'sessions#create'
 
