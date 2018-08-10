7a3064289bbbbbc95e3cde7ea44bb317754a67a9
agpiermarini <piermarini.andrew@gmail.com>
2018-08-10 14:47:24 -0600

    Add delete points job

---
+++ app/jobs/delete_points_job.rb
@@ -0,0 +1,9 @@
+class DeletePointsJob < ApplicationJob
+  queue_as :default
+
+  def perform(scavenger_hunt_id)
+    service = ElasticService.new(scavenger_hunt_id)
+    service.delete_points.deliver_now
+    service.delete_all_user_points.deliver_now
+  end
+end
