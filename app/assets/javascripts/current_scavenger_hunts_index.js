e355bf839edb7ebfb27b25affcd98fcd50bf14a2
agpiermarini <piermarini.andrew@gmail.com>
2018-08-11 19:02:54 -0600

    Remove octothorpe from a tag

--- app/assets/javascripts/current_scavenger_hunts_index.js
+++ app/assets/javascripts/current_scavenger_hunts_index.js
@@ -46,7 +46,7 @@ async function populateCurrentScavengerHunts(username, token, response) {
               <li>Description: ${hunt.description}</li>
             </div>
           </a>
-          <a class="delete-btn" username=${username} token=${token} scavenger_hunt_id=${hunt.scavenger_hunt_id} href="#"><i class="fas fa-trash-alt"></i></a>
+          <a class="delete-btn" username=${username} token=${token} scavenger_hunt_id=${hunt.scavenger_hunt_id} href=""><i class="fas fa-trash-alt"></i></a>
         </ul>`
       )
   })
