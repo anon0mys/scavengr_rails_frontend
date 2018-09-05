40ce47530378e9d53543a9bdf076c4830adcff0f
agpiermarini <piermarini.andrew@gmail.com>
2018-09-05 13:28:38 -0600

    Add delete_single_point job to delete a point for a scavenger hunt and all related user points

--- app/jobs/delete_single_point_job.rb
+++ app/jobs/delete_single_point_job.rb
@@ -4,5 +4,6 @@ class DeleteSinglePointJob < ApplicationJob
   def perform(point_id, scavenger_hunt_id)
     service = ElasticService.new(scavenger_hunt_id)
     service.delete_point(point_id)
+    service.delete_user_point(point_id)
   end
 end
--- app/jobs/delete_single_user_point_job.rb
+++
@@ -1,8 +0,0 @@
-class DeleteSingleUserPointJob < ApplicationJob
-  queue_as :default
-
-  def perform(point_id, scavenger_hunt_id)
-    service = ElasticService.new(scavenger_hunt_id)
-    service.delete_user_point(point_id)
-  end
-end
