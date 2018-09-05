2bd39d45d5acb641bb013317c412d2251cf360ec
agpiermarini <piermarini.andrew@gmail.com>
2018-09-05 15:06:38 -0600

    Rename job. Remove call to delete point method (will now be handled with fetch)

--- app/jobs/delete_corresponding_user_points_job.rb
+++ app/jobs/delete_corresponding_user_points_job.rb
@@ -1,9 +1,8 @@
-class DeleteSinglePointJob < ApplicationJob
+class DeleteCorrespondingUserPointsJob < ApplicationJob
   queue_as :default
 
   def perform(point_id, scavenger_hunt_id)
     service = ElasticService.new(scavenger_hunt_id)
-    service.delete_point(point_id)
     service.delete_user_point(point_id)
   end
 end
