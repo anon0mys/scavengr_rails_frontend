6f67d6739ecb3bfe0cc6ff0c05f9eb4a769043f3
agpiermarini <piermarini.andrew@gmail.com>
2018-09-05 18:28:15 -0600

    Update fetch request with elastic base url and proper auth headers. Update variable names and methods

--- app/assets/javascripts/user_scavenger_hunt_show.js
+++ app/assets/javascripts/user_scavenger_hunt_show.js
@@ -4,49 +4,50 @@ $(document).ready(() => {
 
 function deleteScavengerHuntPoint(event) {
   let pointId = $(this).attr('point_id')
-  let scavengerHuntId = $(this).attr('scavenger_hunt_id')
-  fetch(`localhost:9200/points/_delete_by_query`, {
+  fetch('https://bde23565173445efa24d03df46b00ee1.us-east-1.aws.found.io:9243/points/_delete_by_query', {
       method: 'POST',
       headers: {
-        "Content": "application/json"
+        "Authorization": "Basic ZWxhc3RpYzpPbWtqaksyTm12MWwwaHR6MmpmanN0dTI=",
+        "Content-Type": "application/json"
       },
-      body: `{ "query": { "bool": { "must": { "match": { "_id": ${pointId}  } } } } }`
+      body: `{ "query": { "bool": { "must": { "match": { "_id": "${pointId}"  } } } } }`
   })
-  .then(response => fetchPoints(scavengerHuntId))
+  .then(response => fetchPoints(response.json()))
   .catch(error => console.log(error));
 }
 
-async function fetchPoints(scavengerHuntId) {
+async function fetchPoints(response) {
   await response
-  fetch(`localhost:9200/points/_search`, {
+  fetch(`https://bde23565173445efa24d03df46b00ee1.us-east-1.aws.found.io:9243/points/_search`, {
       method: 'POST',
       headers: {
-        "Content": `application/json`
+        "Authorization": "Basic ZWxhc3RpYzpPbWtqaksyTm12MWwwaHR6MmpmanN0dTI=",
+        "Content-Type": `application/json`
       },
       body: `{"query": {"bool": {"must": {"match_all": {}}}}}`
   })
   .then(response => {
-    let response = response.json()
-    populatePoints(response)
+    let points = response.json()
+    populatePoints(points)
     displayFlashMessage();
   })
   .catch(error => console.log(error));
 }
 
 async function populatePoints(response) {
-  let response = await response
-  let results = response.hits.hits
+  let points = await response
+  let results = points.hits.hits
   $('.cards-container').html("")
   results.forEach(result => {
     $('.cards-container').append(
        `<ul class="points-card">
           <div class="card-left">
-            <li><b>Address:</b><br> ${result.source.point.address}</li>
-              <li><b>Message:</b> ${result.source.point.message}</li>
-              <li><b>Clue:</b> ${result.source.point.clue}</li>
+            <li><b>Address:</b><br> ${result._source.point.address}</li>
+              <li><b>Message:</b> ${result._source.point.message}</li>
+              <li><b>Clue:</b> ${result._source.point.clue}</li>
             </div>
           <a class="edit-btn" data-method="get" href="#"><i class="fas fa-edit"></i></a>
-          <a class="delete-btn delete-scavenger-hunt-point" rel="nofollow" data-method="delete" href="/scavenger_hunts/${result.source.point.scavenger_hunt_id}/points/${point.id}"><i class="fas fa-trash-alt"></i></a>
+          <a class="delete-btn delete-scavenger-hunt-point" rel="nofollow" data-method="delete" href="/scavenger_hunts/${result._source.point.scavenger_hunt_id}/points/${result._source.point.id}"><i class="fas fa-trash-alt"></i></a>
       </ul>`
       )
   })
